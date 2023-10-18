9f15bc7f6345d9c8736fdd893a3b30943e9cb759
armin <alirezayaghobi219@yahoo.com>
2023-10-18 22:35:29 +0330

    add files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/Skills/Skills";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scroll-up/ScrollUp";
+const App = () => {
+  return (
+      <>
+      <Header/>
+          <main className="main">
+              <Home />
+              <About />
+              <Qualification />
+              <Skills />
+              <Services />
+              <Testimonials />
+              <Contact />
+          </main>
+          <Footer />
+          <ScrollUp/>
+      </>
+  )
+}
+export default App;
---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,24 @@
+import Image1 from "../../assets/SNMP.png"
+import Image2 from "../../assets/Othello.png"
+import Image3 from "../../assets/WebPage.png"
+
+export const Data = [
+    {
+        id : 1,
+        image: Image1,
+        title: "Android SNMP Monitoring System",
+        description: "monitor android base phone through SNMP protocol"
+    },
+    {
+        id : 2,
+        image: Image2,
+        title: "Othello game with AI",
+        description: "implement of othello game which has AI agent and users can play with it"
+    },
+    {
+        id : 2,
+        image: Image3,
+        title: "Webpage for support products",
+        description: "implement of support page for Omidtech company api and front panel as well as admin panel"
+    }
+];
